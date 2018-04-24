56cfe3353467aebb639d50741beb8285ee1ef0f8
Matthew J Whitney <matthewjwhitney@gmail.com>
2018-04-24 00:06:25 -0700

    install sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,32 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var pkg = require('./package.json');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Copy third party libraries from /node_modules into /vendor
 gulp.task('vendor', function() {
 
   // Bootstrap
-  gulp.src(['./node_modules/bootstrap/dist/**/*', '!./node_modules/bootstrap/dist/css/bootstrap-grid*', '!./node_modules/bootstrap/dist/css/bootstrap-reboot*']).pipe(gulp.dest('./vendor/bootstrap'))
+  gulp.src([
+      './node_modules/bootstrap/dist/**/*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
+      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
+    ])
+    .pipe(gulp.dest('./vendor/bootstrap'))
 
   // jQuery
-  gulp.src(['./node_modules/jquery/dist/*', '!./node_modules/jquery/dist/core.js']).pipe(gulp.dest('./vendor/jquery'))
+  gulp.src([
+      './node_modules/jquery/dist/*',
+      '!./node_modules/jquery/dist/core.js'
+    ])
+    .pipe(gulp.dest('./vendor/jquery'))
 
   // jQuery Easing
-  gulp.src(['node_modules/jquery.easing/*.js']).pipe(gulp.dest('vendor/jquery-easing'))
+  gulp.src([
+      'node_modules/jquery.easing/*.js'
+    ])
+    .pipe(gulp.dest('vendor/jquery-easing'))
 
 })
 
@@ -30,7 +44,19 @@ gulp.task('browserSync', function() {
 
 // Dev task
 gulp.task('dev', ['browserSync'], function() {
+  gulp.watch('./sass/**/*.scss', ['styles']);
   gulp.watch('./css/*.css', browserSync.reload);
   gulp.watch('./js/*.js', browserSync.reload);
   gulp.watch('./*.html', browserSync.reload);
+});
+
+// Sass task
+gulp.task('styles', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('./css'));
 });
