13802df143aee35b0b59ba9c275f388f17db2a17
Matthew J Whitney <matthewjwhitney@gmail.com>
2018-04-25 19:35:03 -0700

    Add content with grid system

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,61 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var pkg = require('./package.json');
-var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
 
 // Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
+gulp.task("vendor", function() {
 
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
+	// Bootstrap
+	gulp.src([
+		"./node_modules/bootstrap/dist/**/*",
+		"!./node_modules/bootstrap/dist/css/bootstrap-grid*",
+		"!./node_modules/bootstrap/dist/css/bootstrap-reboot*"
+	])
+		.pipe(gulp.dest("./vendor/bootstrap"));
 
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
+	// jQuery
+	gulp.src([
+		"./node_modules/jquery/dist/*",
+		"!./node_modules/jquery/dist/core.js"
+	])
+		.pipe(gulp.dest("./vendor/jquery"));
 
-  // jQuery Easing
-  gulp.src([
-      'node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('vendor/jquery-easing'))
+	// jQuery Easing
+	gulp.src([
+		"node_modules/jquery.easing/*.js"
+	])
+		.pipe(gulp.dest("vendor/jquery-easing"));
 
-})
+});
 
 // Default task
-gulp.task('default', ['vendor']);
+gulp.task("default", ["vendor"]);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
+gulp.task("browserSync", function() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
 });
 
 // Dev task
-gulp.task('dev', ['browserSync'], function() {
-  gulp.watch('./sass/**/*.scss', ['styles']);
-  gulp.watch('./css/*.css', browserSync.reload);
-  gulp.watch('./js/*.js', browserSync.reload);
-  gulp.watch('./*.html', browserSync.reload);
+gulp.task("dev", ["browserSync"], function() {
+	gulp.watch("./sass/**/*.scss", ["styles"]);
+	gulp.watch("./css/*.css", browserSync.reload);
+	gulp.watch("./js/*.js", browserSync.reload);
+	gulp.watch("./*.html", browserSync.reload);
 });
 
 // Sass task
-gulp.task('styles', function () {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('./css'));
+gulp.task("styles", function () {
+	return gulp.src("./sass/**/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(autoprefixer({
+			browsers: ["last 2 versions"],
+			cascade: false
+		}))
+		.pipe(gulp.dest("./css"));
 });
